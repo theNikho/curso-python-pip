26c2b1e33d0fc0e376612be0521d1c1657289e94
Nicolas Agudelo <nicolas.agudelot@gmail.com>
2023-01-19 10:44:31 -0500

    Prueba 3

--- game/main.py
+++ game/main.py
@@ -1,6 +1,7 @@
 import random
 
 # Esto es una prueba
+print("Mi primer juego")
 def choose_options():
   options = ('piedra', 'papel', 'tijera')
   user_option = input('piedra, papel o tijera => ')
